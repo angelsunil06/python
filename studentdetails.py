bfdb3ec67135449a04851bd475c089b34b0b1532
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2024-10-01 14:55:56 +0530

    my fist commit

---
+++ studentdetails.py
@@ -0,0 +1,8 @@
+name=input('enter the student name:')
+rollno=int(input('enter roll no:'))
+rollno=rollno+1
+cgpa=float(input('enter cgpa'))
+percentage_marks=cgpa*10
+print('name of the student is:',name)
+print('rollno of the student:',rollno)
+print('marks in percentage:',percentage_marks,end='%')
