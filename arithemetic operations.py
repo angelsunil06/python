eaf38f8e2af55779bf135f216f1638dabecb7d37
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2024-10-08 15:38:46 +0530

    Add files via upload

---
+++ arithemetic operations.py
@@ -0,0 +1,21 @@
+'''
+author:angel
+date:8 october 2024
+version:1.0
+'''
+
+num1=int(input('enter first number:'))
+num2=int(input('enter second number:'))
+num3=int(input('enter third number:'))
+sum=num1+num2
+print('the sum of num1 and num2:',sum)
+difference=num1-num2
+print('the difference when num2 is subtracted from num1:',difference)
+product=num1*num2
+print('the product of num1 and num2is:',product)
+quotient=num1/num2
+print('the quotient when num1 is divided by num 2:',quotient)
+remainder=num1%num2
+print('the remainder when num1 is divided by num2:',remainder)
+prob=(num1+num2)*num3/2
+print('the result of (num1+num2)*num3/2is:',prob)
---
+++ concatenating strings.py
@@ -0,0 +1,13 @@
+'''
+author: angel
+date: 8
+october
+2024
+version: 1.0
+'''
+firstname=input('enter first name:')
+lastname=input('enter last name:')
+fullname=firstname+' '+lastname
+print(fullname)
+length=len(firstname)
+print(fullname[length+1:])
