2851a65e04250461beaf479e9a95d8de9b556387
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2024-10-15 15:07:12 +0530

    Add files via upload

---
+++ simpleinterest.py
@@ -0,0 +1,11 @@
+'''
+author:angel
+date:14 ctober 2024
+version:1.0
+'''
+principle=int(input('enter principle:'))
+rate=int(input('enter rate:'))
+year=int(input('enter no of year'))
+simpleinterest=principle*rate*year
+print (simpleinterest)
+
