cdfe4da844e6f622a6831f2641da8889e331e19f
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2024-10-15 14:40:32 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,9 @@
+age=int(input('enter the age:'))
+if age<10:
+    print('fare is rupees 7')
+elif age>=10 and age<60:
+    fare=10
+    print('fare is rupees 10')
+else:
+    print('fare is rupees 5')
+
