e71a7166098ffb71192d33f82ff1c1a1c69cb3c8
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2024-12-03 14:33:10 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    first_number=1
+    second_number=0
+    third_number=0
+    while third_number<n:
+        print(third_number,end=' ')
+        third_number=first_number+second_number
+        first_number=second_number
+        second_number=third_number
